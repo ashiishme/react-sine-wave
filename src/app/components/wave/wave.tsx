735b9586c56ce40e7f9ca35c7c73ce286756e976
ashiishme <iamashiishy@gmail.com>
2022-02-04 20:25:36 +0100

    feature(#2): basic time counter to randomly set wave color

--- src/app/components/wave/wave.tsx
+++ src/app/components/wave/wave.tsx
@@ -1,27 +1,38 @@
-import { FC } from 'react';
-import { useCanvasContext } from 'app/hooks/useCanvas';
-import useResponsiveSize from 'app/hooks/useResponsiveSize';
-import { default as WaveObj } from 'app/lib/wave';
+import { FC } from 'react'
+import { useCanvasContext } from 'app/hooks/useCanvas'
+import useResponsiveSize from 'app/hooks/useResponsiveSize'
+import { default as WaveObj } from 'app/lib/wave'
+import useColorGenerator from 'app/hooks/useColorGenerator'
 
 const Wave: FC = () => {
-    const { context } = useCanvasContext();
-    const { width, height } = useResponsiveSize();
-    let frequency = 0.013;
-    const waves = {
-        frontWave: new WaveObj([0.0211, 0.028, 0.015], 'rgba(255,179,0,0.88)'),
-        backWave: new WaveObj([0.0122, 0.018, 0.005], 'rgba(255,179,0,0.48)'),
-    };
+  const { context } = useCanvasContext()
+  const { width, height } = useResponsiveSize()
+  const { generateColor } = useColorGenerator()
 
-    const render = () => {
-        context?.clearRect(0, 0, width, height);
-        Object.entries(waves).forEach(([, wave]) => {
-            wave.draw(context!, width, height, frequency);
-        });
-        frequency += 0.013;
-        requestAnimationFrame(render);
-    };
-    if (context) render();
-    return null;
-};
+  let frequency = 0.013
+  let colors: { [key: string]: string } = generateColor()
+  let timer = 1
+  const waves = {
+    frontWave: new WaveObj([0.0211, 0.028, 0.015], 'rgba(255,179,0,0.88)'),
+    backWave: new WaveObj([0.0122, 0.018, 0.005], 'rgba(255,179,0,0.48)'),
+  }
 
-export default Wave;
+  const render = () => {
+    context?.clearRect(0, 0, width, height)
+    Object.entries(waves).forEach(([waveName, wave]) => {
+      wave.waveColor = colors[waveName]
+      wave.draw(context!, width, height, frequency)
+    })
+    if (timer === 500) {
+      colors = generateColor()
+      timer = 1
+    }
+    timer++
+    frequency += 0.013
+    requestAnimationFrame(render)
+  }
+  if (context) render()
+  return null
+}
+
+export default Wave
